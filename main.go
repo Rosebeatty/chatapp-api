e7412becc8c56d7327646b5671a49dd915a5dd9f
Rose Beazzy <rose.beatty@gmail.com>
2020-10-18 23:59:33 +0200

    created user model, seperate package for mongodb connection, and first route for registering a user

--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
+	"github.com/golang/chatapp/pkg/routes"
+	"github.com/golang/chatapp/pkg/utils"
 	"github.com/golang/chatapp/pkg/websocket"
 	"github.com/gorilla/mux"
 )
 
+func main() {
+	fmt.Println("Chat App v0.01")
+	utils.ConnectDB()
+	r := setupRoutes()
+
+	http.ListenAndServe(":8080", r)
+
+}
+
+func setupRoutes() *mux.Router {
+	router := mux.NewRouter()
+	router.Use(forCORS)
+
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+	router.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		routes.CreateUser(w, r)
+	})
+
+	return router
+}
+
 func forCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		fmt.Println(w.Header())
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
@@ -44,40 +66,4 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 
 	pool.Register <- client
 	client.Read()
-}
-
-func setupRoutes() {
-	router := mux.NewRouter()
-	router.Use(forCORS)
-
-	pool := websocket.NewPool()
-	go pool.Start()
-
-	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(pool, w, r)
-	})
-
-	http.ListenAndServe(":8080", router)
-}
-
-func setupMongoDB() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("<<MongoDB Connection URI>>"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-}
-
-func main() {
-	fmt.Println("Distributed Chat App v0.01")
-	setupRoutes()
-	setupMongoDB()
 }
